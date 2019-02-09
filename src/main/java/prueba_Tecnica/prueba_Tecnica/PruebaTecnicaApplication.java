c2858257391b6a9f3635aade6e145953a70d6a54
Alvaro Javier Osorio Castro - Ceiba Software <alvaro.osorio@ceiba.com.co>
2019-02-09 10:48:02 -0500

    Carga inicial del proyecto

---
+++ src/main/java/prueba_Tecnica/prueba_Tecnica/PruebaTecnicaApplication.java
@@ -0,0 +1,17 @@
+package prueba_Tecnica.prueba_Tecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaApplication.class, args);
+	}
+	
+	
+	
+
+}
+
