4374ad40a604c6621d9ca851cc80c36c6556688c
alvaro.osorio <alvaro.osorio@ALVARO-OSORIO.ceiba.local>
2019-02-13 09:41:44 -0500

    prueba de test

---
+++ src/main/java/com/pruebaTecnica/dominio/Moto.java
@@ -0,0 +1,15 @@
+package com.pruebaTecnica.dominio;
+
+import java.util.Date;
+
+public class Moto extends Vehiculo {
+
+	public Moto(String modelo, String placa, Date fechaHoraIngreso, Date fechaHoraSalida, int tipoVehiculo) {
+		super(modelo, placa, fechaHoraIngreso, fechaHoraSalida, tipoVehiculo);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+
+}
--- src/test/java/com/pruebaTecnica/dominio/ServicioTest.java
+++ src/test/java/com/pruebaTecnica/dominio/ServicioTest.java
@@ -7,29 +7,39 @@ import java.util.Calendar;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.pruebaTecnica.EstacionamientoAplication;
+import com.pruebaTecnica.controller.EstacionamientoController;
 import com.pruebaTecnica.persistencia.ServicioEntity;
+import com.pruebaTecnica.repositorio.IRepositorioServicio;
 import com.pruebaTecnica.servicio.EstacionamientoService;
 import com.pruebaTecnica.utils.Utilitarios;
 
 public class ServicioTest {
 	
+  @Autowired
+  IRepositorioServicio iRepositorioServicio;
 
 
+  /*
 	@Test
 	public void registrarEntradaCarroTest() {
+		ServicioEntity servicioEntity= new ServicioEntity();
+		servicioEntity.setEstado(1);
+		servicioEntity.setModelo("1999");
+		servicioEntity.setPlaca("TSQ");
+		servicioEntity.setTipoVehiculo(1);
 		Utilitarios util= new Utilitarios();
 		EstacionamientoService estacionamientoService = new EstacionamientoService();
 		Factura factura = new Factura();
-		ServicioEntity servicio=new ServicioEntity();
-		servicio.setEstado(util.PARQUEADO);
-		servicio.setModelo("999");
-		servicio.setPlaca("ITB2334");
-		servicio.setTipoVehiculo(util.CARRO);
+	
+		factura = estacionamientoService.registrarEntrada(servicioEntity);
+
 		
-		factura = estacionamientoService.registrarEntrada(servicio);
-		Assert.assertNotNull(factura);
-	}
+		
+		Assert.assertEquals("TSQ", factura.getPlaca());
+	}*/
 
 
 }
