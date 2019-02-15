f6cdbd344842024de6dd87d3454450bc212f29e9
alvaro.osorio <alvaro.osorio@ALVARO-OSORIO.ceiba.local>
2019-02-15 10:46:29 -0500

    cambio en test

--- src/test/java/com/estacionamiento/utils/UtilitariosTest.java
+++ src/test/java/com/estacionamiento/utils/UtilitariosTest.java
@@ -10,95 +10,79 @@ import org.junit.Test;
 
 public class UtilitariosTest {
 
+	@Ignore
+	@Test
+	public void fechaActualAStringTest() throws ParseException {
 
-		
-		
-        @Ignore
-		@Test
-		public void fechaActualAStringTest() throws ParseException{
-              
-		    Date ahora= new Date();
-            String ahoraString="";
-            
-            ahoraString = Utilitarios.fechaActualAString();
-            
+		new Date();
+		Utilitarios.fechaActualAString();
 
-			Assert.assertSame(null,null);
-		
+		Assert.assertSame(null, null);
 
-		}
-	
-		@Test
-		public void fechaStringADateTest() throws ParseException{
-              
-			
-            String fechaString="15-02-2019 08:34:22";
-            
-            Date ahoraString = Utilitarios.fechaStringADate(fechaString);
+	}
 
-			Assert.assertNotNull(ahoraString);
-		
+	@Test
+	public void fechaStringADateTest() throws ParseException {
 
-		}
-		
+		String fechaString = "15-02-2019 08:34:22";
 
-		
-		@Test
-		public void esDomingoTest() throws ParseException{
-              
-			
-           Date domingo=Utilitarios.fechaStringADate("17-02-2019 01:21:22");
-           Calendar calendarDomingo = Calendar.getInstance();
-           calendarDomingo.setTime(domingo);
-           
-           Date lunes=Utilitarios.fechaStringADate("17-02-2019 01:21:22");
-           Calendar calendarLunes = Calendar.getInstance();
-           calendarLunes.setTime(lunes);
-            
-           boolean esDomingo =Utilitarios.esDomingoOLunes(calendarDomingo);
-           boolean esLunes =Utilitarios.esDomingoOLunes(calendarLunes);
+		Date ahoraString = Utilitarios.fechaStringADate(fechaString);
 
-			Assert.assertTrue(esDomingo);
-			Assert.assertTrue(esLunes);
-		
+		Assert.assertNotNull(ahoraString);
 
-		}
-		
-		@Test
-		public void NoesDomingoOLunesTest() throws ParseException{
-              
-			Date sabado=Utilitarios.fechaStringADate("16-02-2019 01:21:22");
-	           Calendar calendarSabado = Calendar.getInstance();
-	           calendarSabado.setTime(sabado);
-           
-            
-           boolean esDomingoOLunes =Utilitarios.esDomingoOLunes(calendarSabado);
+	}
 
-			
-		
-           Assert.assertFalse(esDomingoOLunes);
-		}
-		
-		@Test
-		public void placaVehiculoIniciaPorATest(){
-              
-			   String placa= "AVJ-243";
-	          
-	           boolean placaInicaEnA =Utilitarios.placaVehiculoIniciaPorA(placa);
+	@Test
+	public void esDomingoTest() throws ParseException {
 
-	           Assert.assertTrue(placaInicaEnA);
-		}
-		
-		@Test
-		public void placaVehiculoNoIniciaPorATest(){
-              
-			   String placa= "BVJ-243";
-	          
-	           boolean placaInicaEnA =Utilitarios.placaVehiculoIniciaPorA(placa);
+		Date domingo = Utilitarios.fechaStringADate("17-02-2019 01:21:22");
+		Calendar calendarDomingo = Calendar.getInstance();
+		calendarDomingo.setTime(domingo);
 
-	           Assert.assertFalse(placaInicaEnA);
-		}
-		
-		
+		Date lunes = Utilitarios.fechaStringADate("17-02-2019 01:21:22");
+		Calendar calendarLunes = Calendar.getInstance();
+		calendarLunes.setTime(lunes);
+
+		boolean esDomingo = Utilitarios.esDomingoOLunes(calendarDomingo);
+		boolean esLunes = Utilitarios.esDomingoOLunes(calendarLunes);
+
+		Assert.assertTrue(esDomingo);
+		Assert.assertTrue(esLunes);
+
+	}
+
+	@Test
+	public void NoesDomingoOLunesTest() throws ParseException {
+
+		Date sabado = Utilitarios.fechaStringADate("16-02-2019 01:21:22");
+		Calendar calendarSabado = Calendar.getInstance();
+		calendarSabado.setTime(sabado);
+
+		boolean esDomingoOLunes = Utilitarios.esDomingoOLunes(calendarSabado);
+
+		Assert.assertFalse(esDomingoOLunes);
+	}
+
+	@Test
+	public void placaVehiculoIniciaPorATest() {
+
+		String placa = "AVJ-243";
+
+		boolean placaInicaEnA = Utilitarios.placaVehiculoIniciaPorA(placa);
+
+		Assert.assertTrue(placaInicaEnA);
+	}
+
+	@Test
+	public void placaVehiculoNoIniciaPorATest() {
+
+		String placa = "BVJ-243";
+
+		boolean placaInicaEnA = Utilitarios.placaVehiculoIniciaPorA(placa);
+
+		Assert.assertFalse(placaInicaEnA);
+	}
+
+
 
 }
